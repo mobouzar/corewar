1b3b36366b95287f9d632c0419a11508a12fccdf
mobouzar <mobouzar@student.1337.ma>
2020-11-27 11:01:09 +0100

    fix line clrearing

--- visualizer/menu_viewer.c
+++ visualizer/menu_viewer.c
@@ -6,7 +6,7 @@
 /*   By: mobouzar <mobouzar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 11:22:18 by mobouzar          #+#    #+#             */
-/*   Updated: 2020/11/26 14:55:21 by mobouzar         ###   ########.fr       */
+/*   Updated: 2020/11/27 10:59:04 by mobouzar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,14 +41,12 @@ void	pause_handler(t_visu *visu)
 
 void	event_handler(t_visu *visu)
 {
-	visu->key = event_listenner();
-	if (visu->key > 0)
+	if ((visu->key = event_listenner()) > 0)
 	{
-		if (visu->key == 32)
+		if (visu->key == 32 && (visu->pause = 1))
 		{
 			nodelay(stdscr, TRUE);
 			pause_handler(visu);
-			visu->pause = 1;
 		}
 		else if (visu->key == 27)
 			visu->close = 1;
@@ -57,16 +55,14 @@ void	event_handler(t_visu *visu)
 			visu->cycle_speed -= 20000;
 			visu->speed += 10;
 		}
-		else if (visu->key == 45 && visu->cycle_speed < 5000000 && visu->speed > 0)
+		else if (visu->key == 45 &&\
+			visu->cycle_speed < 5000000 && visu->speed > 0)
 		{
 			visu->cycle_speed += 20000;
 			visu->speed -= 10;
 		}
-		else if (visu->key == 's')
-		{
+		else if (visu->key == 's' && (visu->step = 1))
 			nodelay(stdscr, FALSE);
-			visu->step = 1;
-		}
 	}
 }
 
@@ -78,16 +74,29 @@ void	print_menu(t_visu *visu)
 	mvwprintw(visu->menu, 50, 4, "SPEED        : +/-");
 	mvwprintw(visu->menu, 52, 4, "STEP BY STEP : s");
 	mvwprintw(visu->menu, 54, 4, "QUIT         : esc");
-	mvwprintw(visu->menu, 58, 1, "------------------------ aff ------------------------");
+	mvwprintw(visu->menu, 58, 1,\
+	"------------------------ aff ------------------------");
 	wattroff(visu->menu, A_BOLD);
+}
+
+void	clear_line(t_visu *visu)
+{
+	if (visu->speed == 90 || visu->speed == 0)
+	{
+		wclrtoeol(visu->menu);
+		wattron(visu->menu, COLOR_PAIR(visu->win));
+		box(visu->menu, ACS_VLINE, ACS_HLINE);
+		wborder(visu->menu, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+		wattroff(visu->menu, COLOR_PAIR(visu->win));
+	}
 }
 
 void	menu_handler(t_corewar *war, t_visu *visu)
 {
 	wattron(visu->menu, A_BOLD);
 	mvwprintw(visu->menu, 4, 4, "Cycles/second limit : ");
-	wclrtoeol(visu->menu);
 	mvwprintw(visu->menu, 4, 26, "%d", visu->speed);
+	clear_line(visu);
 	mvwprintw(visu->menu, 7, 4, "Cycle : %d", war->cycle);
 	mvwprintw(visu->menu, 9, 4, "Processes : %d", war->nbr_process);
 	mvwprintw(visu->menu, 11, 4, "CYCLE_TO_DIE : %d", war->cycle_to_die);
